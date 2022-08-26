0e0e0dd4ddc41e1865674a4ec08fffb30e24885d
Sergio (Admethius) <sergio.cp@hotmail.es>
2022-08-26 12:28:56 -0400

    cambio en los dos primeros servicios para que lean la data sin json-server

--- src/app/listador/services/listar.service.ts
+++ src/app/listador/services/listar.service.ts
@@ -1,8 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { IBanco, Surcursal } from '../componentes/banco.interface';
-import { IMoto } from '../componentes/moto.interface';
 import { IPokemon } from '../componentes/pokemon.interface';
 
 @Injectable({
@@ -20,19 +18,7 @@ export class ListarService {
     private apiKey:string="kiekejeoio8383733ujeij3u3u3"
     // Retorna los parámetros que enviaremos al sitio real
     // Esto solo funciona cuando funciona contra el sitio real
-    get httpParamsMoto () {
-      return new HttpParams().set( 'fields'
-                    , 'patente;color;nro_Chasis;max_speed'
-                //, 'name;capital'
-                    ).set("access_key",this.apiKey);
-    }
 
-    get httpParamsBanco () {
-      return new HttpParams().set( 'fields'
-                    , 'nombre;direccion;telefono;sucursales'
-                //, 'name;capital'
-                    ).set("access_key",this.apiKey);
-    }
 
     get httpParamsPokemon () {
       return new HttpParams().set( 'fields'
@@ -41,36 +27,13 @@ export class ListarService {
                     ).set("access_key",this.apiKey);
     }
 
-
-
-   // Método que consulta al back-end por los paises
-    buscarMoto(stBusco:string):Observable<IMoto[]>{
+      // Método que consulta al back-end por los paises
+    buscarPokemon(stBusco:string):Observable<IPokemon[]>{
       // Crea la URl
-      let stUrl = `${this.apiUrl}motos/${stBusco}`
       // Modificamos la url solo para realizar pruebas locales
-      stUrl=`http://localhost:3000/${stBusco}`
+      let stUrl=`http://localhost:3000/pokemon/?id=${stBusco}`
         // Visitamos la url, y eviamos los parámetros
-        return this.http.get<IMoto[]>(stUrl, { params: this.httpParamsMoto })
+        return this.http.get<IPokemon[]>(stUrl, { params: this.httpParamsPokemon })
     }
-
-     // Método que consulta al back-end por los paises
-     buscarBanco(stBusco:string):Observable<IBanco[]>{
-      // Crea la URl
-      let stUrl = `${this.apiUrl}bancos/${stBusco}`
-      // Modificamos la url solo para realizar pruebas locales
-      stUrl=`http://localhost:3000/${stBusco}`
-        // Visitamos la url, y eviamos los parámetros
-        return this.http.get<IBanco[]>(stUrl, { params: this.httpParamsBanco })
-    }
-
-       // Método que consulta al back-end por los paises
-       buscarPokemon(stBusco:string):Observable<IPokemon[]>{
-        // Crea la URl
-        // Modificamos la url solo para realizar pruebas locales
-        let stUrl=`http://localhost:3000/pokemon/?id=${stBusco}`
-          // Visitamos la url, y eviamos los parámetros
-          return this.http.get<IPokemon[]>(stUrl, { params: this.httpParamsPokemon })
-      }
-
 
 }
