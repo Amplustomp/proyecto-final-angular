9c7c44cb07329519d70132c29ca360b19aa03d7b
Sergio (Admethius) <sergio.cp@hotmail.es>
2022-08-26 19:49:51 -0400

    pokedex version final

--- src/app/listador/listar.module.ts
+++ src/app/listador/listar.module.ts
@@ -6,13 +6,12 @@ import { SidebarListarComponent } from './shared/sidebar-listar/sidebar-listar.c
 import { PorBancosComponent } from './por-bancos/por-bancos.component';
 import { PorMotoComponent } from './por-moto/por-moto.component';
 import { PorPokemonComponent } from './por-pokemon/por-pokemon.component';
-import { PokemonTablaComponent } from './componentes/pokemon-tabla/pokemon-tabla.component';
 import { PokemonInputComponent } from './componentes/pokemon-input/pokemon-input.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ModalSucursalesModule } from './shared/modales/modal-sucursales/modal-sucursales.module';
 import { BsModalService } from 'ngx-bootstrap/modal';
-
+import { MaterialModule } from './shared/material/MaterialModule';
 
 
 @NgModule({
@@ -22,15 +21,15 @@ import { BsModalService } from 'ngx-bootstrap/modal';
     PorBancosComponent,
     PorMotoComponent,
     PorPokemonComponent,
-    PokemonTablaComponent,
-    PokemonInputComponent,
+    PokemonInputComponent
   ],
   imports: [
     CommonModule,
     RouterModule,
     FormsModule,
     HttpClientModule,
-    ModalSucursalesModule
+    ModalSucursalesModule,
+    MaterialModule
   ],
   exports: [
     PageListarComponent
